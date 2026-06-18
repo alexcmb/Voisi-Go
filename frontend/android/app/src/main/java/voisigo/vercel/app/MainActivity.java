87fc9589494417b9abc817baf014a6310e32e196
William <berowilliam60@gmail.com>
2026-06-18 15:12:44 +0200

    feat: add lint for backend

---
+++ frontend/android/app/src/main/java/voisigo/vercel/app/MainActivity.java
@@ -0,0 +1,5 @@
+package voisigo.vercel.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'voisigo.vercel.app',
+  appName: 'VoisiGO',
+  webDir: 'dist'
+};
+
+export default config;
